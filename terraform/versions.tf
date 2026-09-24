e6368976ec07971da951e1e63fb82f1f907f620b
romit-98 <romit.vic@gmail.com>
2026-09-24 10:34:27 +1000

    Add Terraform infrastructure workflow with remote state backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,20 @@
 terraform {
-    required_version = ">= 1.7.0"
+  required_version = ">= 1.7.0"
 
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 4.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "sit722-tfstate-rg"
+    storage_account_name = "sit722tfstate224870477"
+    container_name       = "tfstate"
+    key                  = "week10.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
